5656c6681340e7a703e59fa1dd45bc9b09ab1625
svetad <sv.dmytr@gmail.com>
2023-04-20 10:54:23 +0300

    OFF build tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,6 +14,7 @@ cmake -DPYTHON_EXECUTABLE:FILEPATH="$PYTHON" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+      -DXGEMS_BUILD_DEMOS=OFF \
       ..
 # Build step
 make -j${CPU_COUNT}
