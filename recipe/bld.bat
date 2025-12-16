665ea0c972f000a6ed1fef0a97ac35a771ff7236
svetad <sv.dmytr@gmail.com>
2025-12-16 20:22:32 +0200

    Build with gems3k v4.4.4

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,13 +3,18 @@ cd build
 
 cmake -G Ninja ^
       -DXGEMS_PYTHON_INSTALL_PREFIX="%PREFIX%" ^
-      -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
+      -DPython_EXECUTABLE="%PYTHON%" ^
+      -DPython_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
+      -DPython_LIBRARY="%PYTHON_LIB%" ^
       -DXGEMS_BUILD_DEMOS=OFF ^
       -DCMAKE_CXX_FLAGS="/utf-8" ^
       ..
+
 if errorlevel 1 exit 1
+ninja -j 1
 ninja install
 if errorlevel 1 exit 1
+
