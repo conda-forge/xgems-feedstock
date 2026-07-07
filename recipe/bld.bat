bab2bca4812d3f4f772bdad4c5dc6033f74b7b6c
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2026-07-07 08:07:39 +0200

    Update Python executable path in build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,14 @@
 mkdir build
 cd build
 
+set "PYTHON_FWD=%PYTHON:\=/%"
+
 cmake -G Ninja ^
       -DXGEMS_PYTHON_INSTALL_PREFIX="%PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
-      -DPython3_EXECUTABLE="%PYTHON%" ^
+      -DPython3_EXECUTABLE="%PYTHON_FWD%" ^
       -DPython3_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
       -DPython3_LIBRARY="%PYTHON_LIB%" ^
       -DXGEMS_BUILD_DEMOS=OFF ^
