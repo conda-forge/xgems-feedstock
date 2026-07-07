8f38bb6506a16a2be487e74cd6a3490f06519c67
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2026-07-07 07:43:36 +0200

    Update Python variables to use Python3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,9 +6,9 @@ cmake -G Ninja ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
-      -DPython_EXECUTABLE="%PYTHON%" ^
-      -DPython_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
-      -DPython_LIBRARY="%PYTHON_LIB%" ^
+      -DPython3_EXECUTABLE="%PYTHON%" ^
+      -DPython3_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
+      -DPython3_LIBRARY="%PYTHON_LIB%" ^
       -DXGEMS_BUILD_DEMOS=OFF ^
       -DCMAKE_CXX_FLAGS="/utf-8" ^
       ..
@@ -17,4 +17,3 @@ if errorlevel 1 exit 1
 ninja -j 1
 ninja install
 if errorlevel 1 exit 1
-
